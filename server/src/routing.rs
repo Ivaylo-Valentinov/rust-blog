92eb5e9f2993aae7e9bcb6beff118bba3bedb926
Ivaylo Petkov <ivailo2000petkov@gmail.com>
2022-02-15 16:07:10 +0200

    Unify the routes

--- server/src/routing.rs
+++ server/src/routing.rs
@@ -7,9 +7,6 @@ pub fn configuration() -> Box<dyn Fn(&mut web::ServiceConfig)> {
         cfg.
             route("/users", web::post().to(handlers::users::register)).
             route("/auth", web::post().to(handlers::users::login)).
-            service(
-                web::resource("/test").
-                route(web::get().to(handlers::users::something))
-            );
+            route("/test", web::get().to(handlers::users::something));
     })
 }
